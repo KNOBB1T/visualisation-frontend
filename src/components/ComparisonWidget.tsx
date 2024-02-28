692c589dd9edd33afa625df16bc32ca0cd03b928
KNOBB1T <ander.dequadra@gmail.com>
2024-02-28 19:00:34 +0000

    Comparison page styling complete

--- src/components/ComparisonWidget.tsx
+++ src/components/ComparisonWidget.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./ComparisonWidget.css";
 import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 // interface ComparisonWidgetProps {
 //   speciesData: Species[];
@@ -29,18 +31,50 @@ export const ComparisonWidget = () => {
             src={sessionStorage.getItem(key) || undefined}
             alt={`networkImage${index}`}
           />
-          <div>
-            <p className="species-name">
-              {sessionStorage.getItem(key + "CompactName")}
-            </p>
-            <p>{sessionStorage.getItem(key + "Domain")}</p>
-            <p>{sessionStorage.getItem(key + "Evolution")}</p>
-            <p>{sessionStorage.getItem(key + "Nodes")}</p>
-            <p>{sessionStorage.getItem(key + "Edges")}</p>
+          <div className="comparison-tab">
+            <div className="comparison-info">
+              <div className="comparison-header">
+                <p className="species-name">
+                  {sessionStorage.getItem(key + "CompactName")}
+                </p>
+              </div>
+              <div className="comparison-footer">
+                <p>Domain: {sessionStorage.getItem(key + "Domain")}</p>
+                <p>
+                  Evolution:{" "}
+                  {sessionStorage.getItem(key + "Evolution") === "0"
+                    ? "Unknown"
+                    : sessionStorage.getItem(key + "Evolution")}
+                </p>
+                <p>Total Nodes:{sessionStorage.getItem(key + "Nodes")}</p>
+                <p>Total Edges:{sessionStorage.getItem(key + "Edges")}</p>
+              </div>
+            </div>
+            <div>
+              <Button
+                className="remove-comparison"
+                onClick={() => {
+                  sessionStorage.removeItem(key);
+                  sessionStorage.removeItem(key + "CompactName");
+                  sessionStorage.removeItem(key + "Domain");
+                  sessionStorage.removeItem(key + "Evolution");
+                  sessionStorage.removeItem(key + "Nodes");
+                  sessionStorage.removeItem(key + "Edges");
+                  window.location.reload();
+                }}
+              >
+                <FontAwesomeIcon icon={faXmark} className="cancel-comparison" />
+              </Button>
+            </div>
           </div>
         </div>
       ))}
-      <Button className="compare-network-button">Compare Networks</Button>
+      <Button
+        className="compare-network-button"
+        disabled={sessionStorage.length < 12}
+      >
+        Compare Networks
+      </Button>
     </div>
   );
 };
